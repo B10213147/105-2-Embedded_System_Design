3681fc2d6cc712c954228266f609f45dc23fc821
B10213147 <B10213147@yuntech.org.tw>
2017-04-26 13:52:51 +0800

    Finished exercise by using low level

--- week6/Timer(Low Level)/main.c
+++ week6/Timer(Low Level)/main.c
@@ -1,33 +1,44 @@
 #include "stm32f4xx.h"                  // Device header
 
 int main(void){
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
-	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN;	// Enable GPIOA, GPIOC clock
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;	// Enable Timer2 clock
 	
-	GPIOA->MODER |= 0x1U << 2 * 5;
+	GPIOA->MODER |= 0x1U << 2 * 5;	// User LD2: PA5
+	GPIOC->MODER &= ~(0x3U << 2 * 13);	// B1 USER: PC13
 	
-	TIM2->CNT = 0;
-	TIM2->CR1 |= TIM_CR1_CEN;
+	TIM2->CNT = 0;	// Reset Timer2 counter
 	
-	int frequency = 1;	// unit: Hz
+	int frequency = 10;	// Unit: Hz
 	float duty = 0.5;	
-	uint32_t nH = SystemCoreClock / frequency * duty;
-	uint32_t nL = SystemCoreClock / frequency - nH;
-	uint32_t next = TIM2->CNT + nL;
+	int period = SystemCoreClock / frequency;	// Unit: cycle
+	uint32_t nH = period * duty;
+	uint32_t nL = period - nH;	
 	
 	int pin = 0;
-	GPIOA->BSRR = 0x1U << (5 + 16);
+	GPIOA->BSRR = 0x1U << (5 + 16);	// Reset PA5
 	while(1){
-		if(TIM2->CNT > next){
-			if(pin != 0){
-				next += nL;
-				GPIOA->BSRR = 0x1U << (5 + 16);
-			}
-			else{
-				next += nH;
-				GPIOA->BSRR = 0x1U << 5;
-			}
-			pin ^= 1;
+		if(!(GPIOC->IDR & GPIO_IDR_IDR_13)){	// Button pressed
+			TIM2->CR1 |= TIM_CR1_CEN;	// Enable Timer2 counter
+			uint32_t next = TIM2->CNT + nL;			
+			do{
+				if(TIM2->CNT > next){
+					if(pin != 0){	// High period timeout
+						next += nL;
+						GPIOA->BSRR = 0x1U << (5 + 16);	// Reset PA5
+					}
+					else{	// Low period timeout
+						next += nH;
+						GPIOA->BSRR = 0x1U << 5;	// Set PA5
+					}
+					pin ^= 1;
+				}
+			}while(!(GPIOC->IDR & GPIO_IDR_IDR_13));	// Button pressed
+			
+			pin = 0;
+			GPIOA->BSRR = 0x1U << (5 + 16);	// Reset PA5
+			TIM2->CR1 &= ~TIM_CR1_CEN;
+			TIM2->CNT = 0;	// Reset Timer2 counter
 		}
 	}
 	
