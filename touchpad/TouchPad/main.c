d0fb76d2ef89fcb5639e19ab53ee6228ba8094df
B10213147 <B10213147@yuntech.org.tw>
2017-04-30 02:18:13 +0800

    Added functionality
    TP1 frequency added 1
    TP4 frequency subtracted 1
    TP13 duty added 0.1
    TP16 duty subtracted 0.1
    
    Changed TIM3 as delay timer

--- touchpad/TouchPad/main.c
+++ touchpad/TouchPad/main.c
@@ -6,10 +6,20 @@
 void startup(void);
 void delay_us(int n);
 void delay_ms(int n);
+void functionality(void);
 
+int frequency = 1;
+float duty = 0.5;
+uint32_t nL, nH, next;
+int pin = 0;
 uint8_t keys[16];
 int main(void){
 	startup();
+	
+	int period = SystemCoreClock / 16000 / frequency;
+	nH = period * duty;
+	nL = period - nH;
+	next = TIM_GetCounter(TIM3) + nL;
 	
 	while(1){
 		if(GPIO_ReadInputDataBit(GPIOC, 3)){
@@ -21,8 +31,18 @@ int main(void){
 				GPIO_WriteBit(GPIOC, 2, 0);
 				delay_us(5);	
 			}
+			functionality();
 		}
-		//delay_ms(5);
+		if(TIM_GetCounter(TIM2) > next){
+			if(pin != 0){
+				next += nL;
+			}
+			else{
+				next += nH;
+			}
+			pin ^= 1;
+			GPIO_WriteBit(GPIOA, 5, pin);
+		}
 	}
 	
 	return 0;
@@ -30,33 +50,70 @@ int main(void){
 
 void startup(void){
 	RCC_AHB1PeriphClockCmd(RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOCEN, ENABLE);
-	RCC_APB1PeriphClockCmd(RCC_APB1ENR_TIM2EN, ENABLE);
+	RCC_APB1PeriphClockCmd(RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN, ENABLE);
 
 	GPIO_Set_Direction(GPIOA, 0, 0x0020);
 	GPIO_Set_Direction(GPIOC, 0x0008, 0x0004);
 	//GPIOC->PUPDR |= (0x1 << 2 * 2) | (0x1 << 2 * 3);
 	GPIOC->PUPDR |= (0x1 << 2 * 3);
-	TIM_SetCounter(TIM2, 0);
-}
-
-void delay_us(int n){
-	TIM_PrescalerConfig(TIM2, 16 - 1);
-	TIM_GenerateEvent(TIM2, TIM_EGR_UG);	
-	TIM_Cmd(TIM2, ENABLE);
-
-	while(TIM2->CNT < n);
-	TIM2->CNT = 0;
 	
-	TIM_Cmd(TIM2, DISABLE);
-}
-
-void delay_ms(int n){
+	TIM_SetCounter(TIM3, 0);
+	TIM_SetCounter(TIM2, 0);
 	TIM_PrescalerConfig(TIM2, 16000 - 1);
 	TIM_GenerateEvent(TIM2, TIM_EGR_UG);	
 	TIM_Cmd(TIM2, ENABLE);
+}
 
-	while(TIM2->CNT < n);
-	TIM2->CNT = 0;
+void functionality(void){
+	int i;
+	for(i = 0; keys[i] == 0; i++);
+	switch(i){
+		case 0:
+			if(frequency < 10){
+				frequency++;
+			}
+			break;
+		case 3:
+			if(frequency > 1){
+				frequency--;
+			}
+			break;
+		case 12:
+			if(duty < 0.90){
+				duty += 0.1;
+			}
+			break;
+		case 15:
+			if(duty > 0.1){
+				duty -= 0.1;
+			}
+			break;
+		default:
+			break;
+	}
+	int period = SystemCoreClock / 16000 / frequency;
+	nH = period * duty;
+	nL = period - nH;
+}
+
+void delay_us(int n){
+	TIM_PrescalerConfig(TIM3, 16 - 1);
+	TIM_GenerateEvent(TIM3, TIM_EGR_UG);	
+	TIM_Cmd(TIM3, ENABLE);
+
+	while(TIM3->CNT < n);	
 	
-	TIM_Cmd(TIM2, DISABLE);
+	TIM_Cmd(TIM3, DISABLE);
+	TIM3->CNT = 0;
+}
+
+void delay_ms(int n){
+	TIM_PrescalerConfig(TIM3, 16000 - 1);
+	TIM_GenerateEvent(TIM3, TIM_EGR_UG);	
+	TIM_Cmd(TIM3, ENABLE);
+
+	while(TIM3->CNT < n);	
+	
+	TIM_Cmd(TIM3, DISABLE);
+	TIM3->CNT = 0;
 }
