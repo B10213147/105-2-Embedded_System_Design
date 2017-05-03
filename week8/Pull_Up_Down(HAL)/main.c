c2335271355ff2ff8116af4bcd749709b3bacde2
B10213147 <B10213147@yuntech.org.tw>
2017-05-03 19:56:43 +0800

    Added PC10 as interrupt source

--- week8/Pull_Up_Down(HAL)/main.c
+++ week8/Pull_Up_Down(HAL)/main.c
@@ -7,7 +7,7 @@ void startup(void);
 
 int frequency = 5;	// unit: Hz
 int duty = 50;	// unit: %
-uint32_t nH, nL, next;
+uint32_t nH, nL;
 int main(void){
 	startup();	
 	while(1);
@@ -16,14 +16,24 @@ int main(void){
 }
 
 void EXTI15_10_IRQHandler(void){
-	if((EXTI->PR & EXTI_PR_PR13) != 0){
-		frequency = frequency % 10 + 1;
-		int period = SystemCoreClock / 16000 / frequency;	// Unit: cycle
-		nH = period * (float)duty / 100;
-		nL = period - nH;
+	if((EXTI->PR & EXTI_PR_PR10) != 0){
+		if(frequency > 1){
+			frequency--;
+		}
 		
-		EXTI->PR = EXTI_PR_PR13;
+		EXTI->PR = EXTI_PR_PR10;	// Clear pending bit
 	}
+	else if((EXTI->PR & EXTI_PR_PR13) != 0){
+		if(frequency < 10){
+			frequency++;
+		}
+		
+		EXTI->PR = EXTI_PR_PR13;	// Clear pending bit
+	}
+	
+	int period = SystemCoreClock / 16000 / frequency;	// Unit: cycle
+	nH = period * (float)duty / 100;
+	nL = period - nH;
 }
 
 int pin = 0;
@@ -39,7 +49,7 @@ void TIM2_IRQHandler(void){
 		pin ^= 1;
 		GPIO_WriteBit(GPIOA, 5, pin);
 	
-		TIM2->SR &= ~TIM_SR_CC1IF;
+		TIM2->SR &= ~TIM_SR_CC1IF;	// Clear pending bit
 	}
 }
 
@@ -48,7 +58,8 @@ void startup(void){
 	RCC_APB1PeriphClockCmd(RCC_APB1ENR_TIM2EN, ENABLE);
 	
 	GPIO_Set_Direction(GPIOA, 0, 0x0020);
-	GPIO_Set_Direction(GPIOC, 0x2000, 0);
+	GPIO_Set_Direction(GPIOC, 0x2400, 0);
+	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR10_0;
 	
 	pin = 0;
 	GPIO_WriteBit(GPIOA, 5, pin);
@@ -65,8 +76,9 @@ void startup(void){
 	TIM_Cmd(TIM2, ENABLE);
 	
 	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	SYSCFG->EXTICR[2] |= SYSCFG_EXTICR3_EXTI10 & SYSCFG_EXTICR3_EXTI10_PC;
 	SYSCFG->EXTICR[3] |= SYSCFG_EXTICR4_EXTI13 & SYSCFG_EXTICR4_EXTI13_PC;
-	EXTI->IMR |= EXTI_IMR_MR13;
-	EXTI->FTSR |= EXTI_FTSR_TR13;
+	EXTI->IMR |= EXTI_IMR_MR10 | EXTI_IMR_MR13;
+	EXTI->FTSR |= EXTI_FTSR_TR10 | EXTI_FTSR_TR13;
 	NVIC_EnableIRQ(EXTI15_10_IRQn);	
 }
